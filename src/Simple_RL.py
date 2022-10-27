5e7fca6b9b1f00e68dfb2b4c74b7a11fcd31f702
Hayley Owens <hayleyjeanowens@gmail.com>
2022-10-27 09:57:38 -0400

    Commenting plans

---
+++ src/Simple_RL.py
@@ -0,0 +1,9 @@
+# States: dimension x dimension grid
+# Actions: [Ask_For_Guidance, Go_Left, Go_Right, Go_Up, Go_Down]
+# Rewards: No rewards for taking action? Maybe -1, -100 if 100 turns pass and not done. +100 if at terminating state
+
+# Create grid
+
+# Q-Learning MDP
+
+# Create and display q-table
