c8ad7014edb121d30acb09645af02d78d0fd4613
Hayley Owens <hayleyjeanowens@gmail.com>
2022-12-03 17:37:53 -0500

    Updated with turtlebot code

--- src/catkin_ws/src/moving_turtlebot/scripts/move.py
+++ src/catkin_ws/src/moving_turtlebot/scripts/move.py
@@ -1,5 +1,8 @@
+#!/usr/bin/env python
+
 import rospy
 from geometry_msgs.msg import Twist
+from math import pi
 
 
 if __name__ == '__main__':
@@ -7,20 +10,35 @@ if __name__ == '__main__':
     cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
     move_cmd = Twist()
     
-    while True:
-        parsing = True
-        while parsing:
-            input = input("Give a direction: ")
-            if input.lower() in["up", "u"]:
-                move_cmd.linear.x = -0.5
-                move_cmd.angular.z = 0
-            elif input.lower() in["down", "d"]:
-                move_cmd.linear.x = 0.5
-                move_cmd.angular.z = 0
-            elif input.lower() in["right", "r"]:
-                move_cmd.linear.y = 0.5
-                move_cmd.angular.z = 0
-            elif input.lower() in["left", "l"]:
-                move_cmd.linear.y = -0.5
-                move_cmd.angular.z = 0
-        cmd_vel.publish(move_cmd)
+    duration = 5
+    linear_speed = 0.2
+    angular_speed = pi*4/4
+    r = rospy.Rate(10)
+
+    parsing = True
+    while parsing:
+        input_dir = raw_input("Give a direction: ")
+        if input_dir.lower() in ["up", "u"]:
+            print("In Up")
+            move_cmd.linear.x = -linear_speed
+            move_cmd.angular.z = 0
+            cmd_vel.publish(move_cmd)
+            rospy.sleep(duration)
+        elif input_dir.lower() in ["down", "d"]:
+            move_cmd.linear.x = linear_speed
+            move_cmd.angular.z = 0
+            cmd_vel.publish(move_cmd)
+            rospy.sleep(duration)
+        elif input_dir.lower() in ["right", "r"]:
+            move_cmd.angular.z = -angular_speed
+            move_cmd.linear.x = 0
+            cmd_vel.publish(move_cmd)
+            rospy.sleep(duration)
+        elif input_dir.lower() in ["left", "l"]:
+            move_cmd.angular.z = angular_speed
+            move_cmd.linear.x = 0
+            cmd_vel.publish(move_cmd)
+            rospy.sleep(duration)
+        elif input_dir.lower() == "x":
+            parsing = False
+        
